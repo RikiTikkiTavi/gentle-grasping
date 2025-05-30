f7e6f30d29c355389269ae050bededcd85e9995f
Prokhorkot <kotprokhor@gmail.com>
2025-05-30 20:41:09 +0200

    Added nested mlflow runs (issue #13)

--- src/gentle_grasp/train.py
+++ src/gentle_grasp/train.py
@@ -8,8 +8,10 @@ from gentle_grasp.model_module import GentleGraspModelModule
 from gentle_grasp.data_module import GentleGraspDataModule
 
 import hydra
+import mlflow
 
 from torchmetrics import Accuracy
+from sklearn.model_selection import KFold
 
 @hydra.main(version_base="1.3", config_path="../../configs", config_name="train")
 def main(cfg: dict):
@@ -19,34 +21,47 @@ def main(cfg: dict):
 
     torch.set_float32_matmul_precision("high")
 
-    datamodule = GentleGraspDataModule(data_path=data_path, batch_size=batch_size, num_workers=2, val_ratio=0.2, cv=cfg.cv)
+    # Initialize MLflow
+    
+    with mlflow.start_run(run_name="cv_parent", nested=False):
+        for fold_id in range(cfg.cv.n_folds if cfg.cv.enabled else 1):
+            with mlflow.start_run(run_name=f"Fold {fold_id}", nested=True):
 
-    modelmodule = GentleGraspModelModule(cfg=cfg)
+                datamodule = GentleGraspDataModule(
+                    data_path=data_path, 
+                    batch_size=batch_size, 
+                    num_workers=2, 
+                    val_ratio=0.2, 
+                    cv=cfg.cv,
+                    fold=fold_id
+                )
 
-    # Callbacks
-    early_stop = EarlyStopping(monitor="val_loss", patience=20, mode="min")
+                modelmodule = GentleGraspModelModule(cfg=cfg)
 
-    # Logger
-    logger = MLFlowLogger(
-        tracking_uri=mlflow_tracking_uri,
-        experiment_name="gentle_grasping_experiment",
-        run_name="action_conditional_model_run",
-    )
+                # Callbacks
+                early_stop = EarlyStopping(monitor="val_loss", patience=20, mode="min")
 
-    # Log hyperparameters
-    logger.log_hyperparams(cfg)
+                # Logger
+                logger = MLFlowLogger(
+                    tracking_uri=mlflow_tracking_uri,
+                    experiment_name="gentle_grasping_experiment",
+                    run_name="action_conditional_model_run",
+                )
 
-    # Trainer
-    trainer = pl.Trainer(
-        max_epochs=50,
-        callbacks=[early_stop],
-        logger=logger,
-        accelerator="auto",
-        devices=[6],
-        enable_checkpointing=False,
-    )
+                # Log hyperparameters
+                logger.log_hyperparams(cfg)
 
-    trainer.fit(modelmodule, datamodule=datamodule)
+                # Trainer
+                trainer = pl.Trainer(
+                    max_epochs=50,
+                    callbacks=[early_stop],
+                    logger=logger,
+                    accelerator="auto",
+                    devices=[6],
+                    enable_checkpointing=False,
+                )
+
+                trainer.fit(modelmodule, datamodule=datamodule)
 
 
 if __name__ == "__main__":
