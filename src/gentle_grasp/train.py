d6915c0bcae108f2618fad278b3b13f6162d6c16
Prokhorkot <kotprokhor@gmail.com>
2025-06-17 11:30:55 +0200

    DEBUG: tracking the sound tensor shape

--- src/gentle_grasp/train.py
+++ src/gentle_grasp/train.py
@@ -38,7 +38,7 @@ def main(cfg: OmegaConf):
             "post": hydra.utils.instantiate(cfg.transforms.sensor.post),
         },
         # TODO: Specify sound transforms
-        "audo": [hydra.utils.instantiate(i) for i in cfg.transforms.audio]
+        "audio": [hydra.utils.instantiate(i) for i in cfg.transforms.audio]
     }
 
     torch.set_float32_matmul_precision("high")
